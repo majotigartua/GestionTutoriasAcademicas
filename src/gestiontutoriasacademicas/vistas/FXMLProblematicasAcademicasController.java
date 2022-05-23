ba0d9cb9104f14d84cc2fa073f9439c5a9b13a5c
María José Torres Igartua <57418909+majotigartua@users.noreply.github.com>
2022-05-23 16:44:07 -0500

    Add files via upload

---
+++ src/gestiontutoriasacademicas/vistas/FXMLProblematicasAcademicasController.java
@@ -0,0 +1,24 @@
+package gestiontutoriasacademicas.vistas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLProblematicasAcademicasController implements Initializable {
+
+    @FXML
+    private Label labelInstruccion;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clicButtonCancelar(ActionEvent event) {
+    }
+    
+}
