4b24f1024da33c6ae0ba0309922d9fe2b0da206f
María José Torres Igartua <57418909+majotigartua@users.noreply.github.com>
2022-05-23 16:43:36 -0500

    Delete src/gestiontutoriasacademicas/util directory

--- src/gestiontutoriasacademicas/util/Constantes.java
+++
@@ -1,9 +0,0 @@
-package gestiontutoriasacademicas.util;
-
-public class Constantes {
-    
-    public static final int CODIGO_CREDENCIALES_INCORRECTAS = 1;
-    public static final int CODIGO_OPERACION_CORRECTA = 2;
-    public static final int CODIGO_ERROR_CONEXION_BASE_DATOS = 3;
-    
-}
