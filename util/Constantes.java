ea408331d22decd6455b4348d70c7bb051650ab2
María José Torres Igartua <57418909+majotigartua@users.noreply.github.com>
2022-05-23 16:42:16 -0500

    Delete util directory

--- util/Constantes.java
+++
@@ -1,9 +0,0 @@
-package gestiontutoriasacademicas.util;
-
-public class Constantes {
-    
-    public static final int CODIGO_CREDENCIALES_INCORRECTAS = 1;
-    public static final int CODIGO_INICIO_SESION_CORRECTO = 2;
-    public static final int CODIGO_ERROR_CONEXION_BASE_DATOS = 3;
-    
-}
