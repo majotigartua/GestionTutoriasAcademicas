584c6365cb3da7e2a13af8dbf6968eb2f9ff1e0b
María José Torres Igartua <57418909+majotigartua@users.noreply.github.com>
2022-05-23 19:24:54 -0500

    Add files via upload

--- src/gestiontutoriasacademicas/util/Constantes.java
+++ src/gestiontutoriasacademicas/util/Constantes.java
@@ -2,6 +2,10 @@ package gestiontutoriasacademicas.util;
 
 public class Constantes {
     
+    public static final int ID_ROL_COORDINADOR_TUTORIAS_ACADEMICAS = 1;
+    public static final int ID_ROL_JEFE_CARRERA = 2;
+    public static final int ID_ROL_ADMINISTRADOR = 3;
+    
     public static final int CODIGO_CREDENCIALES_INCORRECTAS = 1;
     public static final int CODIGO_INICIO_SESION_CORRECTO = 2;
     public static final int CODIGO_ERROR_CONEXION_BASE_DATOS = 3;
