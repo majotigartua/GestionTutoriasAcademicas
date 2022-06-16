b87f86d752c5162365eb2e345e4c69b314bd21b7
María José Torres Igartua <57418909+majotigartua@users.noreply.github.com>
2022-06-16 15:17:05 -0500

    Delete src/gestiontutoriasacademicas directory

--- src/gestiontutoriasacademicas/interfaces/NotificacionComentarioGeneral.java
+++
@@ -1,9 +0,0 @@
-package gestiontutoriasacademicas.interfaces;
-
-import gestiontutoriasacademicas.modelo.pojo.ComentarioGeneral;
-
-public interface NotificacionComentarioGeneral {
-    
-    public abstract void notificarComentarioGeneral(ComentarioGeneral comentarioGeneral);
-    
-}
--- src/gestiontutoriasacademicas/interfaces/NotificacionProblematicaAcademica.java
+++
@@ -1,9 +0,0 @@
-package gestiontutoriasacademicas.interfaces;
-
-import gestiontutoriasacademicas.modelo.pojo.ProblematicaAcademica;
-
-public interface NotificacionProblematicaAcademica {
-   
-    public abstract void notificarProblematicaAcademica(ProblematicaAcademica problematicaAcademica);
-    
-}
